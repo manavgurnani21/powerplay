d6438f882d343bc898c8818ddc440752012d90ae
Manav Gurnani <mgurnani@ucdavis.edu>
2026-03-23 15:30:19 -0700

    part 2 & 3 complete

--- pipeline_design/merchant_fraud_detection.py
+++ pipeline_design/merchant_fraud_detection.py
@@ -8,10 +8,27 @@ from collections import defaultdict
 
 # ----------------------------- Part I: Count Fraudulent Transactions -----------------------------
 
-fraud_codes = ["stolen_card", "do_not_honor"]
+fraud_codes = ["stolen_card", "do_not_honor"] # codes for fraud transactions
+
+mccs = ["retail,0.5", "airline,3"] # merchant category codes to thresholds
+merchants = ["merch_001,retail", "merch_002,airline", "merch_003,airline"] # merchant to category codes
+minimum_transactions = 4 # minimum transactions to be evaluated
 
 merchant_entries = defaultdict(dict)
 other_entries = []
+
+def merchantMCCMapping(merchants: list[str],mccs: list[str]) -> dict:
+	mccs_dict = dict()
+	merchant_dict = dict()
+	for mcc in mccs: # updating MCCs into dictionary
+		[category, threshold] = mcc.split(",")
+		mccs_dict[category] = float(threshold) if "." in threshold else int(threshold)
+
+	for merchant in merchants: # using dict to format merchant entries
+		[merch_id, category] = merchant.split(",")
+		merchant_dict[merch_id] = mccs_dict[category]
+
+	return dict(sorted(merchant_dict.items()))
 
 def transactionProcessor(transactions: list[str]):
 	for entry in transactions:
@@ -19,43 +36,49 @@ def transactionProcessor(transactions: list[str]):
 		if len(metadata) < 4:
 			other_entries.append(entry)
 			continue
-		merchant_id = metadata[0].split("_")[-1]
-		if metadata[3] in fraud_codes:	
-			if "failure_transactions" not in merchant_entries[merchant_id].keys():
-				merchant_entries[merchant_id]["failure_transactions"] = []
+		merchant_id = metadata[0]
+		if "failure_count" not in merchant_entries[merchant_id].keys():
+			merchant_entries[merchant_id]["failure_count"] = 0
+		if metadata[3] in fraud_codes:
+			merchant_entries[merchant_id]["failure_count"] += 1
 
-			merchant_entries[merchant_id]["failure_transactions"].append({
-				"transaction_id": metadata[1],
-				"amount": metadata[2]
-			})
-		elif metadata[3] == "success":
-			if "success_transactions" not in merchant_entries[merchant_id].keys():
-				merchant_entries[merchant_id]["success_transactions"] = []
+		if "transactions" not in merchant_entries[merchant_id].keys():
+				merchant_entries[merchant_id]["transactions"] = []
 
-			merchant_entries[merchant_id]["success_transactions"].append({
-				"transaction_id": metadata[1],
-				"amount": metadata[2]
-			})
-		else:
-			other_entries.append(entry)
-			continue
+		merchant_entries[merchant_id]["transactions"].append({
+			"transaction_id": metadata[1],
+			"amount": metadata[2]
+		})
 		
 def transactionAggregates():
-	aggregates = []
+	flagged = []
 	for merchant_id in merchant_entries.keys():
-		num_fraud_transactions = len(merchant_entries[merchant_id]["failure_transactions"])
-		if num_fraud_transactions >= 1:
-			aggregates.append(f"merch_{merchant_id},{num_fraud_transactions}")
-	return aggregates
+		num_fraud_transactions = merchant_entries[merchant_id]["failure_count"]
+		num_transactions = len(merchant_entries[merchant_id]["transactions"])
+		if merchant_id not in merchant_mcc.keys() or num_transactions < minimum_transactions:
+			print("Skipping this transaction...")
+			continue
+		if type(merchant_mcc[merchant_id]) is int:
+			# handling threshold
+			if num_fraud_transactions >= merchant_mcc[merchant_id]:
+				flagged.append(merchant_id)
+		elif type(merchant_mcc[merchant_id]) is float:
+			# handling ratio
+			if (num_fraud_transactions / num_transactions) >= merchant_mcc[merchant_id]:
+				flagged.append(merchant_id)
+	return flagged
 
 transactions = [
-    "merch_001,tx_1,100,success",
-    "merch_001,tx_2,200,stolen_card",
-    "merch_002,tx_3,150,insufficient_funds",
-    "merch_001,tx_4,300,do_not_honor",
-    "merch_002,tx_5,50,stolen_card"
+    "merch_001,tx_1,100,stolen_card",
+    "merch_001,tx_2,200,success",
+    "merch_001,tx_3,150,stolen_card",
+    "merch_001,tx_4,300,success",
+    "merch_002,tx_5,50,stolen_card",
+    "merch_002,tx_6,50,stolen_card"
 ]
 
+merchant_mcc = merchantMCCMapping(merchants,mccs)
+print(merchant_mcc)
 transactionProcessor(transactions)
 print(merchant_entries)
 print(other_entries)
