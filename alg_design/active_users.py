cfc88cca9d2ea6af18bdd00b21967d621b1f796e
Manav Gurnani <mgurnani@ucdavis.edu>
2026-03-24 22:17:27 -0700

    finishing part 2

--- alg_design/active_users.py
+++ alg_design/active_users.py
@@ -6,6 +6,7 @@ Description: Given a list of (user_id, login_timestamp) tuples, return all users
 """
 
 from collections import defaultdict
+from datetime import date, timedelta
 
 # Given: List of login events [(user_id: str, login_date: str)]
 # Login Date Format: YYYY-MM-DD (multiple possible logins per day)
@@ -20,24 +21,34 @@ def activeUsers(logins: list[tuple[str, str]], k: int) -> list[str]:
 		return []
 	
 	# for each login attempt
-	for (login_id, date) in logins:
+	for (login_id, login_date) in logins:
 		if login_id not in login_directory.keys():
 			login_directory[login_id] = set()
-		login_directory[login_id].add(date)
+		date_split = login_date.split("-")
+		year = int(date_split[0])
+		month = int(date_split[1])
+		day = int(date_split[2])
+		date_obj = date(year, month, day)
+		login_directory[login_id].add(date_obj)
 	
 	for user in login_directory.keys():
-		if len(login_directory[user]) >= k:
-			active.append(user)
+		login_dates_sorted = sorted(login_directory[user]) # sort the set of dates
+		print(login_dates_sorted)
+		# check for the following:
+			# the kth element should be (oldest + k)
+		for i in range(len(login_dates_sorted)-k+1):
+			print(login_dates_sorted[i+k-1])
+			print(login_dates_sorted[i] + timedelta(days=k-1))
+			if login_dates_sorted[i+k-1] == login_dates_sorted[i] + timedelta(days=k-1) and user not in active:
+				active.append(user)
 
 	return active
 	
 logins = [
     ("alice", "2024-01-01"),
-    ("alice", "2024-01-01"),
-    ("alice", "2024-01-03"),
-    ("alice", "2024-01-05"),
-    ("bob", "2024-01-01"),
-    ("bob", "2024-01-02"),
+    ("alice", "2024-03-10"),
+    ("alice", "2024-03-11"),
+    ("alice", "2024-03-12"),
 ]
 
-print(activeUsers(logins, k=2))
+print(activeUsers(logins, k=4))
